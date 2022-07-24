3b3dc1f11f88e07c0025956ef8bb71411e643f89
Virender Nehra <virender.nehra@vts.com>
2022-07-24 18:58:30 +0530

    add space in about page

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,9 +1,9 @@
 import aboutusImg from '../assets/aboutus.png';
 function AboutUs() {
   return (
-    <div name="about"  className=' py-16 w-full flex items-center flex-col'>
+    <div name="about"  className=' py-8 w-full flex items-center flex-col'>
       <h1 className='text-4xl mt-4 font-semibold'>About us</h1>
-      <div className='grid md:grid-cols-2  m-auto pb-16 gap-8'>
+      <div className='grid md:grid-cols-2  m-auto pb-16 gap-8 py-16'>
         <div className='flex flex-col justify-center md:items-start w-full px-2'>
         <p className='text-xl max-w-screen-sm'>Vnetix provides technology and business process consulting services to enterprise and mid-sized companies, with a focus on Microsoft Dynamics 365.
           We specialize in large and complex implementations where customers want to leverage Dynamics 365 as a strategy to transform their businesses.
