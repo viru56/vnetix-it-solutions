4da1220c63addb79952cc40aad784100abff408b
Vijayeshoo7 <107911018+Vijayeshoo7@users.noreply.github.com>
2022-08-14 13:08:46 +0530

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,7 +7,7 @@ function Banner() {
       <div className='flex justify-center items-center flex-col text-center'>
         <h1 className='text-3xl mb-8 font-bold sm:text-5xl'>Vnetix IT Solutions</h1>
         <p className='max-w-screen-md text-xl'>
-          Vnetix provides technology and business process consulting services to enterprise and mid-sized companies, with a focus on Microsoft Dynamics 365. 
+          VNETIX provides technology and business process consulting services to enterprise and mid-sized companies, with a focus on Microsoft Dynamics 365. 
           We specialize in large and complex implementations where customers want to leverage Dynamics 365 as a strategy to transform their businesses. 
           We provide customized solutions as per the needs of the customer to help them succeed in their business. </p>
       </div>
