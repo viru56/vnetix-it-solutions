d0069a7cb24774d750009d91a003691c7ffbdef2
Vijayeshoo7 <107911018+Vijayeshoo7@users.noreply.github.com>
2022-08-14 13:41:56 +0530

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ function Banner() {
     <div name="home" className='h-screen w-full border-b-2 border-sky-800 bg-sky-200 flex items-center justify-between'>
     <div className='p-4 w-full min-h-[250px]'>
       <div className='flex justify-center items-center flex-col text-center'>
-        <h1 className='text-3xl mb-8 font-bold sm:text-5xl'>Vnetix IT Solutions</h1>
+        <h1 className='text-3xl mb-8 font-bold sm:text-5xl'>VNETIX IT Solutions</h1>
         <p className='max-w-screen-md text-xl'>
           VNETIX provides technology and business process consulting services to enterprise and mid-sized companies, with a focus on Microsoft Dynamics 365. 
           We specialize in large and complex implementations where customers want to leverage Dynamics 365 as a strategy to transform their businesses. 
